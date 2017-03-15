06f53fac31061ce9b77e77375a39c1f4a5cb46b8
docwilmot <docwilmot@gmail.com>
2017-03-15 14:19:00 -0500

    Fix API docs

--- currency.api.php
+++ currency.api.php
@@ -1,10 +1,6 @@
-The currency module provides two callable API functions that allow other
-modules to do real time currency exchange.
+<?php
 
-The functions are:
-
-function currency_api_convert($currency_from, $currency_to, $amount = 1)
-
+/**
  * This function converts two currencies using exchange rates from Yahoo Finance.
  * The currency codes are standard ISO 3-letter codes, and you can find the details
  * here:
@@ -15,9 +11,9 @@ function currency_api_convert($currency_from, $currency_to, $amount = 1)
  *   $from = 'CAD';
  *   $to   = 'USD';
  *   $amt  = 20;
- *   $ret  = currency_api_convert($from, $to, $amt);
+ *   $ret  = currency_convert($from, $to, $amt);
  *   if ($ret['status'] == FALSE) {
- *     drupal_set_message(t('An error occured: '). $ret['message']);
+ *     backdrop_set_message(t('An error occured: '). $ret['message']);
  *   }
  *   else {
  *     print $amt . ' ' . $from . ' = ' . $ret['value'] . ' ' . $to;
@@ -45,9 +41,12 @@ function currency_api_convert($currency_from, $currency_to, $amount = 1)
  *    $result['timestamp'] - Timestamp of the last update to the rates
  *    $result['date'] - Date of the last update to the rates (Format is "m/d/yyyy")
  *    $result['time'] - Time of the last update to the rates (Format is "h:mmpm")
+ */
+function currency_convert($currency_from, $currency_to, $amount = 1) {
+  
+}
 
-function currency_api_get_desc($currency)
-
+/**
  * This function gets the currency name for a standard ISO 3-letter codes,
  * You can find the details here:
  *  http://www.oanda.com/site/help/iso_code.shtml
@@ -58,7 +57,7 @@ function currency_api_get_desc($currency)
  *   $ret = currency_get_description($ccode);
  *   if ($ret == FALSE)
  *   {
- *     drupal_set_message(t('Could not get description'));
+ *     backdrop_set_message(t('Could not get description'));
  *   }
  *   else
  *   {
@@ -71,9 +70,12 @@ function currency_api_get_desc($currency)
  * @return $result
  *   Contains FALSE if the currency cannot be found, otherwise, it
  *   has the description.
+ */
+function currency_get_desc($currency) {
+  
+}
 
-function currency_api_get_symbol($currency)
-
+/**
  * This function gets the currency symbol for a standard ISO 3-letter codes,
  * You can find the details here:
  *  http://www.oanda.com/site/help/iso_code.shtml
@@ -81,9 +83,9 @@ function currency_api_get_symbol($currency)
  * Here is an example on how to use it:
  *
  *   $ccode = 'CAD';
- *   $ret = currency_api_get_symbol($ccode);
+ *   $ret = currency_get_symbol($ccode);
  *   if ($ret == FALSE) {
- *     drupal_set_message(t('Could not get symbol'));
+ *     backdrop_set_message(t('Could not get symbol'));
  *   }
  *   else {
  *     print $ccode .' => '. $ret;
@@ -95,7 +97,14 @@ function currency_api_get_symbol($currency)
  * @return $result
  *   Contains FALSE if the currency symbol cannot be found, otherwise, it
  *   has the symbol.
+ */
+function currency_get_symbol($currency) {
+  
+}
 
-function currency_api_get_list()
-
+/**
  * Returns a list of supported currencies
+ */
+function currency_get_list() {
+  
+}
